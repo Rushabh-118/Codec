f779d78faa8bb2c237834e88759ee5b1b3867f0f
Rushabh 118 <rushabhmistry118@gmail.com>
2025-07-05 16:16:54 +0530

    refactor Loader component for improved styling and layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,15 @@ import Signup from "./components/Signup";
 import ProtectedRoute from "./middleware/protectedRoute";
 
 // Loader component
-const Loader = () => (
-  <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
-    <div className="animate-spin rounded-full h-60 w-60 border-t-4 border-b-4 border-blue-500"></div>
-  </div>
-);
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex flex-col gap-4 w-full h-full items-center justify-center z-50 bg-white bg-opacity-60">
+      <div className="w-44 h-44 border-[14px] border-transparent text-blue-400 text-6xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+        <div className="w-32 h-32 border-[10px] border-transparent text-red-400 text-4xl animate-spin flex items-center justify-center border-t-red-400 rounded-full" />
+      </div>
+    </div>
+  );
+}
 
 // Success component with redirect and toast
 const Success = () => {

11da3baf177484cfe238dfbc410267246fe6db3b
Rushabh 118 <rushabhmistry118@gmail.com>
2025-08-10 09:30:47 +0530

    error solved

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Testimonials from "../components/Testimonials";
 import CTA from "../components/CTA";
-import Pricing from "../components/Pricing";
+import Pricing from "../components/pricing";
 import FAQ from "../components/FAQ";
 import FeedbackForm from "../components/FeedbackForm";
 import Footer from "../components/Footer";

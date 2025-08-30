1690e2c2e062d462f5bcae92bd0b20ed94021c18
Rushabh 118 <rushabhmistry118@gmail.com>
2025-08-30 10:11:04 +0530

    updeted navbar

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -125,7 +125,7 @@ const Navigation = () => {
                 </motion.div>
                 <span className="text-gray-600 dark:text-white font-bold">
                   Hi,{" "}
-                  <span className="underline font-semibold text-blue-500">
+                  <span className="font-semibold text-blue-500">
                     {user.name.split(" ")[0] || user.email?.split("@")[0]}
                   </span>
                 </span>
